0c05b25bdd61a7f6064755d50f4aedec3fe8e226
vignesh <vigneshdotnet@gmail.com>
2021-07-09 05:49:33 +0800

    build

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -3,25 +3,23 @@ const { ModuleFederationPlugin } = require('webpack').container;
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package');
 
-const devConfig = {
-  mode: "development",
-  devServer: {
-    port: "8080",
-    historyApiFallback: {
-      index: "index.html",
-    },
+const DOMAIN_URL = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: "production",
+  output:{
+    filename: '[name].[contenthash].js'
   },
   plugins:[
       new ModuleFederationPlugin({
         name: 'container',
         remotes: {
-          marketing: 'marketing@http://localhost:8081/remoteEntry.js'
+          marketing: `marketing@${DOMAIN_URL}/marketing/remoteEntry.js`
         },
         shared: packageJson.dependencies,
       }),
-      
   ]
 };
 
 
-module.exports = merge(commonConfig, devConfig);
+module.exports = merge(commonConfig, prodConfig);
